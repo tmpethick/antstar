949c5639edacf0f8c33628e49448f3f8fcc74e4a
Thomas Pethick <tmpethick@gmail.com>
2018-04-13 11:17:05 +0200

    Tests SAsimple1

--- AntStar/AntStar/Program.fs
+++ AntStar/AntStar/Program.fs
@@ -6,30 +6,43 @@ open Domain
 open Grid
 open Search
 
+let printPossibleOutcomes state = Grid.allValidActions '0' state 
+                                  |> List.map (fun (_, s') -> printfn "%O" s') 
+                                  |> ignore
+
 let rec testActions state = function
     | action :: actions ->
         match apply action state with
-        | Error err      -> printfn "%O" err
+        | Error err      -> failwith (err.ToString ())
         | Success state' -> printfn "%O" state'
                             testActions state' actions
     | [] -> printfn "Is Goal state: %O" (allGoalsMet state)
 
+let testMinimal () = 
+    let problem = SokobanProblem ("./levels/SAminimal.lvl", Search.allGoalsMet)
+    let actions = [Move ('0', E); Move ('0', E); Push ('0', E, E)]
+    testActions problem.Initial actions
+
+let testSimpleSearch () = 
+    let problem = SokobanProblem ("./levels/SAsimple1.lvl", Search.allGoalsMet)
+    match graphSearch problem with
+    | None -> failwith "SASsimple1 solution not found"
+    | Some _ -> ()
+
 [<EntryPoint>]
 let main argv =
-  let problem = SokobanProblem ("./levels/SAminimal.lvl", Search.allGoalsMet)
+    testMinimal ()
+    testSimpleSearch ()
 
-  printfn "%O" problem.Initial
-  Grid.allValidActions '0' problem.Initial |> List.map (fun (_, s') -> printfn "%O" s') |> ignore
+    let problem = SokobanProblem ("./levels/SAsimple1.lvl", Search.allGoalsMet)
+    printfn "%O" problem.Initial
 
-  let actions = [Move ('0', E); Move ('0', E); Push ('0', E, E)]
-  testActions problem.Initial actions
-
-  match graphSearch problem with
-  | Some solution -> solution 
+    match graphSearch problem with
+    | Some solution -> solution 
                      |> Seq.map (fun n -> n.state) 
                      |> Seq.iter (printf "%O")
-  | None -> printf "No solution found"
+    | None -> printf "No solution found"
 
-  printfn "Press any key to exit"
-  Console.ReadKey() |> ignore
-  0 // return an integer exit code
+    printfn "Press any key to exit"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
